91734d7160a79c40010ad552fcbcb02959137d32
wgosse <we.gosse@gmail.com>
2018-10-07 13:51:43 +0200

    first release with withings client login

--- src/main/java/com/wgcorp/mywithingsweight/MyWithingsWeightController.java
+++ src/main/java/com/wgcorp/mywithingsweight/MyWithingsWeightController.java
@@ -2,21 +2,26 @@ package com.wgcorp.mywithingsweight;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletResponse;
 
-//@RestController
+@RestController
 public class MyWithingsWeightController {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(MyWithingsWeightController.class);
 
-//    @GetMapping("/login")
-    public ResponseEntity<String> facebookLogin(@RequestParam("code") String code, @RequestParam("state") String state,
-                                        HttpServletResponse httpServletResponse) {
+    @Autowired
+    private OAuth2AuthorizedClientService authorizedClientService;
+
+    @GetMapping("/")
+    public ResponseEntity<String> checklogin(OAuth2AuthenticationToken authentication) {
         LOGGER.info("You are logged!");
         return ResponseEntity.ok("LOGGED");
     }
