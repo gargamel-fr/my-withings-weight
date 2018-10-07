385f43664e4bd650af12ece0bd98eed4ffcc8807
wgosse <we.gosse@gmail.com>
2018-10-07 18:54:25 +0200

    custom configuration for security

---
+++ src/main/java/com/wgcorp/mywithingsweight/SecurityConfig.java
@@ -0,0 +1,55 @@
+package com.wgcorp.mywithingsweight;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
+import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+
+import java.util.Collection;
+import java.util.Map;
+
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private OAuth2UserService customUserService;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .anyRequest().authenticated()
+                .and()
+                .oauth2Login().userInfoEndpoint().userService(customUserService);
+    }
+
+    @Bean
+    public OAuth2UserService getOauth2UserService() {
+        return new OAuth2UserService() {
+            @Override
+            public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
+                return new OAuth2User() {
+                    @Override
+                    public Collection<? extends GrantedAuthority> getAuthorities() {
+                        return null;
+                    }
+
+                    @Override
+                    public Map<String, Object> getAttributes() {
+                        return null;
+                    }
+
+                    @Override
+                    public String getName() {
+                        return "wgosse";
+                    }
+                };
+            }
+        };
+    }
+}
